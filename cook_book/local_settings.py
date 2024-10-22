46da6970acc807ac7202a35dd6f1f153d981363d
IzaDobrosz <izabela.dobrosz@gmail.com>
2024-10-22 16:45:11 +0200

    parameters for internationalization added

--- cook_book/local_settings.py
+++ cook_book/local_settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -10,10 +13,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
