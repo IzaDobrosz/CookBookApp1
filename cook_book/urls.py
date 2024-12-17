50fb8ecc8bb4df104f98b5db7b113cb44ef56ee3
IzaDobrosz <izabela.dobrosz@gmail.com>
2024-12-17 17:27:58 +0100

    new paths added

--- cook_book/urls.py
+++ cook_book/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from recipes.models import Recipe
-from recipes.views import TagListView, TagDetailView, RecipeListView, \
-    RecipeDetailView, RecipeStepsView
+from recipes.views import TagListView, TagDetailView, RecipeListView, RecipeDetailView, RecipeStepDetailView, \
+    RecipeAllStepsListView, CommentListCreateView, LandingPageView, FavoriteRecipesView, GenerateRecipePDFView, \
+    LoginView, LogoutView
 from recipes import views
 
 urlpatterns = [
@@ -28,10 +30,17 @@ urlpatterns = [
     path('tag/<int:pk>/', TagDetailView.as_view(), name='tag-detail'),
     path('recipes/', RecipeListView.as_view(), name='recipe-list'),
     path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe-detail'),
-    path('recipe/<int:recipe_id>/steps/<int:step>/', RecipeStepsView.as_view(), name='recipe-steps'),
-    path('recipe/<int:recipe_id>/steps/', RecipeStepsView.as_view(), name='recipe-steps'),
+    path('recipe/<int:recipe_id>/comments/', CommentListCreateView.as_view(), name='comment-list-create'),
+    path('recipe/<int:recipe_id>/steps/<int:step>/', RecipeStepDetailView.as_view(), name='recipe_step_detail'),
+    path('recipe/<int:recipe_id>/steps/', RecipeAllStepsListView.as_view(), name='recipe_steps'),
     path('', views.api_root),
     path('users/', views.UserList.as_view(), name='user-list'),
     path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('', LandingPageView.as_view(), name='landing_page_data'),
+    path('favorites/', FavoriteRecipesView.as_view(), name='favorite_recipes'),
+    path('recipe/<int:recipe_id>/pdf/', GenerateRecipePDFView.as_view(), name='generate_recipe_pdf'),
+    # path('favorites/pdf/', GenerateFavoritesPDFView.as_view(), name='generate_favorites_pdf'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
 ]
