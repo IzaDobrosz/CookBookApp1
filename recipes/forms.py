1ea219f6b675dc5301ea951a9b2c5ba053b95b5f
IzaDobrosz <izabela.dobrosz@gmail.com>
2024-12-17 18:29:21 +0100

    added form for registarion of recipe steps

--- recipes/forms.py
+++ recipes/forms.py
@@ -5,7 +5,7 @@ class RecipeStepForm(forms.ModelForm):
     step_number = forms.IntegerField(min_value=1, max_value=50, label='Step Number')
     instruction = forms.CharField(widget=forms.Textarea, label='Instruction')
     temperature = forms.IntegerField(min_value=1, max_value=300, required=False, label='Temperature')
-    time = forms.IntegerField(required=False, label='Time (minutes)')
+    time = forms.DurationField(required=False, label='Time (minutes)')
 
     class Meta:
         model = RecipeStep
