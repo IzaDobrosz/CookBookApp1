478afdfe0a4e42ca3ed1e0bc31f37e4f8dcea470
IzaDobrosz <izabela.dobrosz@gmail.com>
2024-12-17 18:36:42 +0100

    updated RecipeStepSerializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.reverse import reverse
 from .models import Tag, Recipe, Comment, RecipeStep
 
 
@@ -11,22 +12,48 @@ class TagSerializer(serializers.HyperlinkedModelSerializer):
 
 class RecipeSerializer(serializers.HyperlinkedModelSerializer):
     tags = TagSerializer(many=True, read_only=True)
+    # steps_url = serializers.SerializerMethodField()  # Declare steps_url as a custom field
 
     class Meta:
         model = Recipe
-        fields = ['url', 'id', 'name', 'description', 'prep_time', 'total_time', 'servings', 'ingredients', 'tools', 'preparation_steps', 'type_of_dish', 'preparation_method','ingredient_type','preparation_time','difficulty_level', 'tags']
+        fields = [
+            'url', 'id', 'name', 'description', 'prep_time', 'total_time',
+            'servings', 'ingredients', 'tools', 'preparation_steps', 'type_of_dish',
+            'preparation_method','ingredient_type','preparation_time','difficulty_level',
+            'created_on', 'updated_on','tags'
+        ]
 
+    # def get_steps_url(self, obj):
+    #     """Generate the URL for the steps of certain recipe."""
+    #     request = self.context.get('request')  # Get the request context for absolute URL
+    #     return reverse('recipe_steps', kwargs={'recipe_id': obj.id}, request=request)
 
-class RecipeStepSerializer(serializers.ModelSerializer):
+class RecipeStepSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.SerializerMethodField()
+    time = serializers.SerializerMethodField()    # Przesyłaj czas w sekundach
+
     class Meta:
         model = RecipeStep
-        fields = ['recipe', 'step_details']
+        fields = ['url', 'recipe_id', 'step_number', 'instruction', 'temperature', 'time']
 
+    def get_url(self, obj):
+        """Generate the URL for the steps of a certain recipe."""
+        request = self.context.get('request')  # Get the request context for absolute URL
+        return reverse('recipe_step_detail',
+                       kwargs={'recipe_id': obj.recipe.id, 'step': obj.step_number},
+                       request=request)
+
+    def get_time(self, obj):
+        """Convert timedelta to total seconds."""
+        if obj.time:
+            return int(obj.time.total_seconds())  # Konwertuj timedelta na sekundy
+        return None
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Comment
-        fields = ['id', 'recipe', 'user', 'content', 'created_on']
+        fields = ['id', 'recipe', 'user', 'comment', 'created_on']
         read_only_fields = ['user', 'created_on']
 
 
