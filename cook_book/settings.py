088424429ab86c2189cac623620159f001967297
IzaDobrosz <izabela.dobrosz@gmail.com>
2024-12-17 18:27:37 +0100

    added set up for customized User model

--- cook_book/settings.py
+++ cook_book/settings.py
@@ -28,6 +28,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'recipes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -35,8 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'recipes',
+    'rest_framework.authtoken',
     'corsheaders',
+    # 'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +140,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+AUTH_USER_MODEL = 'recipes.User'
